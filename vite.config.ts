b76406d07bca9257ba40c1b9ef3e74bfc6081227
Ivan Petropolsky <ivan.petropolsky@gmail.com>
2025-12-31 19:17:57 +0100

    fixup! master Add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/2025/',
+  server: {
+    port: 3000,
+    host: true,
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
